c2622ec118c826fd8b1b06943090c8a1c7d67470
deviloppeur24 <louaniakli@outlook.fr>
2024-04-10 15:02:28 -0400

    fichiers de données placés dans le dépôt github

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-# essai
-# essai de push et pull sur R
-# fichier "README"
+# les fichiers de codes R se trouvent dans "traitement_données"
+# les fichiers bruts avec les données sont dans "csv_et_shp"
--- traitement_données/import_données.R
+++ traitement_données/import_données.R
@@ -3,8 +3,11 @@ library(readxl)
 library(rvest)
 library(sf)
 
+######## fichier pour observation de toutes les données de travail dispo'
+
+
 # directory avec toutes les tables xlsx des données du IBGE
-setwd("C:/Users/louan/Desktop/sf_datas/brazil")
+setwd("C:/Users/louan/Desktop/projet_brésil/csv_et_shp")
 
 # indicateurs sociaux au brésil
 # https://www.ibge.gov.br/estatisticas/sociais/trabalho/9221-sintese-de-indicadores-sociais.html
@@ -18,14 +21,59 @@ work.pop.region <- read_xls("Tabela 1.2 (Indic_Geo).xls", skip = 2)
 # tabela 1.3 (IndicSexCor_BR), ta
 work.pop.sex.col <- read_xls("Tabela 1.3 (IndicSexCor_BR).xls", skip = 2)
 
+# tabela 1.3 (IndicSexCor_BR), ta
+work.pop.sex.col <- read_xls("Tabela 1.3 (IndicSexCor_BR).xls", skip = 2)
+
+# tabela 1.4 ; rendecement moyen par mois & par heure, sexe/race
+work.revenu <- read_xls("Tabela 1.4 (Indic_BR_Rend).xls", skip = 2)
+
+# tabela 1.5 ; revenu moyen selon region
+revenu.region <- read_excel("Tabela 1.5 (Indic_Geo_Rend).xls")
+
+# tabela 1.6 ; 
+indicsexcor <- read_excel("Tabela 1.6 (IndicSexCor_BR_Rend).xls")
+
+# tabela 1.7 ; secteur d'emploi, homme/femme/total
+# nb absolu et proportions
+ativcaract <- read_excel("Tabela 1.7 (AtivCaract_BR).xls")
+
+# tabela 1.8
+ativpos <- read_excel("Tabela 1.8 (AtivPos_BR).xls")
+
+# tabela 1.10
+# "carteira de trabalho" : document de preuve d'emploi (accès à la sécurité)
+# https://en.wikipedia.org/wiki/Employment_record_book
+pos_br <- read_excel("Tabela 1.10 (Pos_BR).xls")
+
+# tabela 1.12 :  
+pos_geo <- read_excel("Tabela 1.12 (Pos_Geo).xls")
+
+# tabela 1.14
+ocupcaract <- read_excel("Tabela 1.14 (OcupCaract_Geo).xls")
+
+# tabela 1.15 ; distribution du niveau d'instruction (sexe)
+instructCaract <- read_excel("Tabela 1.16 (InstrCaract).xls")
+
+# tabela.1.8
+instr_geo <- read_excel("Tabela 1.18 (Instr_Geo).xls")
+
+# tabela 1.22
+cargos.geo <- read_excel("Tabela 1.22 (Cargos_Geo).xls")
+
+# tabela 1.23
+formalcar <- read_excel("Tabela 1.23 (FormalCaract_Geo).xls")
 
 
 
+# tabela 1.40 ; sous utilisation de la main d'oeuvre (col2 = total)
+sub_util <- read_excel("Tabela 1.40 (Subutil_Geo).xls")
 
 
-# fichier contenant les municipalités
-pib.munic <- read_excel("C:/Users/louan/Desktop/sf_datas/brazil/pib.municipalite.2010-2021.xlsx")
+
+# pib par municipalité du pays, pour produire carte sf
+pib.munic <- read_excel("pib.municipalite.2010-2021.xlsx")
 
 # pour 2021
 pib.2021 <- pib.munic %>%
   filter(Ano == "2021")
+
